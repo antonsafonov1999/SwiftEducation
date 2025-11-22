e2fb8f66edb35a6c6648bde14c4a551ee593a134
Anton <antonsafonov1999@gmail.com>
2025-11-22 15:37:20 +0100

    ...

--- swiftProject /Education/Education/EducationClass.swift
+++ swiftProject /Education/Education/EducationClass.swift
@@ -60,3 +60,138 @@ class User{
     
 }
  
+
+func sum(_ mass:[Int], clouser: (_ x: Int) ->  Bool)
+{
+    for (i , y) in mass.enumerated()
+    {
+        if clouser(y)
+        {
+            print("\(i) - \(y)")
+        }
+    }
+}
+
+//sum([1, 3, 7, 8, 11, 13, 20]) { x in
+//    return x % 2 == 0
+//}
+
+class teacher
+{
+    let name: String
+    var description: String { return "Hi My name is \(name)!"}
+    
+    init(name: String)
+    {
+        self.name = name
+    }
+    func Work() {print("Hi, I'm working!, \nMy Descriprion is \(self.description)")}
+    func a(){print("b")}
+    
+}
+class IOSDeveloper: teacher
+{
+    override var description: String { return "Hi My name is \(name), and I'm iOS developer!"}
+    
+    override init(name: String)
+    {
+        super.init(name: name)
+        
+    }
+    override func a(){print("a")}
+}
+
+let teacher1: teacher = teacher(name: "Anton")
+let teacher2: IOSDeveloper = IOSDeveloper(name: "Petr")
+let teacher3: teacher = IOSDeveloper(name: "bob")
+let teacher4: teacher = IOSDeveloper(name: "tom")
+
+
+
+func generic<T>(_ mass: [T]) -> Void{
+   print("\(mass)")
+}
+
+
+class App
+{
+    var a: Int = 0
+    func DoSum(a: Int, b: Int) -> Int
+    {
+        return a + b
+    }
+}
+
+extension App
+{
+    func DoSum2(a: Int, b: Int) -> Int
+    {
+        return a + b
+    }
+}
+
+
+class Pesron : App
+{
+    override func DoSum(a: Int, b: Int) -> Int
+    {
+        return a + b + 100
+    }
+}
+struct Size{
+    var width: Int = 2, height: Int = 1
+}
+struct Point
+{
+    var x: Int, y: Int
+}
+struct Rectangle
+{
+    var size = Size()
+    var size2 = Size(width: 2, height: 3)
+    
+}
+
+
+
+//mark: DELEGATE
+protocol DelegateTeacher: AnyObject
+{
+    func TakeHomeWork()
+    func TakeTest()
+}
+class Student
+{
+    var name: String = ""
+    weak var delegate: DelegateTeacher?
+    init(name: String)
+    {self.name = name}
+    
+    func HomeWokr()
+    {delegate?.TakeHomeWork()}
+    
+    func Examen()
+    {delegate?.TakeTest()}
+}
+
+class Teacher: DelegateTeacher
+{
+    func TakeHomeWork() {
+        print("Take home work")
+    }
+    
+    func TakeTest() {
+        print("Take test")
+    }
+    
+    
+}
+
+var student1 = Student(name: "Anton")
+var student2 = Student(name: "Petr")
+
+var tech = Teacher()
+
+//student1.delegate = tech
+//student1.Examen( )
+//student1.HomeWokr()
